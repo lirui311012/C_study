4724dae4b495454430919923cfba1c6328852d0a
liruiWindows <lirui311012@163.com>
2020-10-16 18:57:57 +0800

    更新了不带引用计数的智能指针，推荐使用unique_ptr

--- C++高级/C++智能指针/02_不带引用计数的智能指针.cpp
+++ C++高级/C++智能指针/02_不带引用计数的智能指针.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include  <memory>  //使用智能指针，需要包含该头文件<memory>
 using namespace std;
@@ -5,15 +6,16 @@ using namespace std;
 /*
 不带引用计数的智能指针
 auto_ptr,scoped_ptr,unique_ptr
+不带引用计数的智能指针里面：推荐使用unique_ptr
 */
 
 
 
 template <typename T>
-class CSmartPtr 
+class CSmartPtr
 {
 public:
-	CSmartPtr(T *ptr)
+	CSmartPtr(T* ptr)
 		:mptr(ptr) {}
 	~CSmartPtr() { delete mptr; }
 	T& operator*() { return *mptr; }
@@ -21,25 +23,33 @@ public:
 private:
 	T* mptr;
 };
+////////////////////////////////////////////////////////
 
-int main() {
-#if 0
+
+int main01() {
+
 	CSmartPtr<int> p1(new int);
+	cout << "===========" << endl;
 	CSmartPtr<int> p2(p1);  //拷贝构造
 	//程序直接崩溃
-#endif
-	/*
-	当前这个自定义智能指针面临的一个问题就是，怎么解决浅拷贝问题？
-		解决方案：
-		1.不带引用计数  
-		2.带引用计数
-	*/
 
 	/*
-	不带引用计数的智能指针
-	auto_ptr:C++库里面
-	scoped_ptr:C++11新标准-
-	unique_ptr:C++11新标准
+	当前这个自定义智能指针面临的一个问题就是，怎么解决浅拷贝问题???
+		解决方案：
+		1.不带引用计数
+		2.带引用计数
+	*/
+	return 0;
+}
+
+////////////////////////////////////////////////////////
+//auto_ptr
+int main02() {
+
+	/*不带引用计数的智能指针
+	auto_ptr:C++库里面------不推荐使用
+	scoped_ptr:C++11新标准--很少用
+	unique_ptr:C++11新标准--推荐使用
 	*/
 
 	auto_ptr<int> ptr1(new int);
@@ -80,5 +90,63 @@ int main() {
 	ptr1底层指针为nullptr
 	*/
 
+
 	return 0;
 }
+
+
+
+
+////////////////////////////////////////////////////////
+/*
+scoped_ptr
+//c++11新语法 void test() = delete将这个函数删除掉
+scoped_ptr(const scoped_ptr<T>&) = delete;
+scoped_ptr<T>& operator=(const scoped_ptr<T>&) = delete;
+删除了拷贝构造和operator=运算符重载函数
+*/
+//不带引用计数的智能指针---推荐使用unique_ptr
+/*
+unique_ptr
+unique_ptr也将operator=和拷贝构造函数进行了delete函数删除
+unique_ptr(const unique_ptr<T>&) = delete;
+unique_ptr<T>& operator=(const unique_ptr<T>&) = delete;
+unique_ptr( unique_ptr<T>&& src);  //右值引用
+unique_ptr<T>& operator=( unique_ptr<T>&& src);
+//unique_ptr提供了带右值引用参数的operator=和拷贝构造函数
+
+有如下操作：
+template<typename T>
+unique_ptr<T> getSmartPtr(){
+	unique_ptr<T> ptr(new int);
+	return ptr;
+}
+unique_ptr<int> ptr1 = getSmartPtr<int>();
+
+ptr1 = getSmartPtr<int>();
+
+
+*/
+int main() {
+	unique_ptr<int> ptr1(new int);
+	//unique_ptr<int> ptr2(ptr1);  //编译器报错：尝试引用已删除的函数
+
+
+	//std::move ==>c++11 右值引用  std::move()得到当前变量的右值类型
+	//这样就是在调用unique_ptr的带有右值引用参数的拷贝构造函数
+	unique_ptr<int> ptr2(std::move(ptr1));  //ptr1不再持有资源
+	/*
+	仔细观察，unique_ptr<int> ptr2(ptr1);报错
+	unique_ptr<int> ptr2(std::move(ptr1));虽然这样操作之后，ptr1失去了资源持有权限
+	但是这是在用户可以感知到的情况下来进行的，std::move(ptr1)返回右值引用
+	再看一下auto_ptr，它支持auto_ptr<int> ptr2(ptr1);，然后底层将资源给了ptr2
+	，并且ptr1底层指针为nullptr,在用户无法感知的情况下，将资源剥夺，不好！很危险
+	*/
+	return 0;
+}
+
+
+/*
+结论：
+不带引用计数的智能指针推荐使用unique_ptr!!!
+*/
